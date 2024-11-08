207e05007ef995b42c3dce477770402e6832e0bf
ImJustRob <robertalothian@hotmail.com>
2024-11-07 20:39:03 -0500

    init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Header } from "./Header";
+import { ExercisePage } from "./ExercisePage";
+import { Footer } from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <ExercisePage />
+      <Footer />
+    </div>
+  )
+}
+
+export default App;
