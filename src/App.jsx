cac0a6073c8d581a4ea59c864454e3b9a86c743c
ImJustRob <robertalothian@hotmail.com>
2024-11-09 13:31:26 -0500

    added react router, added login, logout, and signup

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,39 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 import { Header } from "./Header";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 import { ExercisesPage } from "./ExercisesPage";
 import { Footer } from "./Footer";
 
+const router = createBrowserRouter([
+  {
+    element: (
+      <div>
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <ExercisesPage />,
+      },
+      {
+        path: "/signup",
+        element: <SignupPage />,
+      },
+      {
+        path: "/login",
+        element: <LoginPage />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <div>
-      <Header />
-      <ExercisesPage />
-      <Footer />
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
 export default App;
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,15 @@
+import { Link } from "react-router-dom";
+
+// ...
+
+
 export function Header() {
     return (
       <header>
         <nav>
           <a href="#">Home</a> | <a href="#">Link</a>
         </nav>
+          <Link to="/signup">Signup</Link>
       </header>
     )
   }
