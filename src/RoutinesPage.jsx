79924239c8054aa90bb461fceb1c5be2bc72572a
ImJustRob <robertalothian@hotmail.com>
2024-11-13 21:40:20 -0500

    add search bar for routines page

--- src/RoutinesPage.jsx
+++ src/RoutinesPage.jsx
@@ -10,6 +10,7 @@ export function RoutinesPage() {
   const [exercises, setExercises] = useState([]);
   const [isRoutinesShowVisible, setIsRoutinesShowVisible] = useState(false);
   const [currentRoutine, setCurrentRoutine] = useState({});
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleIndex = () => {
     console.log("handleIndex");
@@ -27,41 +28,41 @@ export function RoutinesPage() {
     });
   };
 
-     const handleShow = (routine) => {
-         console.log("handleShow", routine);
-         setIsRoutinesShowVisible(true);
-         setCurrentRoutine(routine);
-       };
+  const handleShow = (routine) => {
+    console.log("handleShow", routine);
+    setIsRoutinesShowVisible(true);
+    setCurrentRoutine(routine);
+  };
 
-          const handleUpdate = (id, params, successCallback) => {
-             console.log("handleUpdate", params);
-             axios.patch(`http://localhost:3000/routines/${id}.json`, params).then((response) => {
-               setRoutines(
-                 routines.map((routine) => {
-                   if (routine.id === response.data.id) {
-                     return response.data;
-                   } else {
-                     return routine;
-                   }
-                 })
-               );
-               successCallback();
-               handleClose();
-             });
-           };
+  const handleUpdate = (id, params, successCallback) => {
+    console.log("handleUpdate", params);
+    axios.patch(`http://localhost:3000/routines/${id}.json`, params).then((response) => {
+      setRoutines(
+        routines.map((routine) => {
+          if (routine.id === response.data.id) {
+            return response.data;
+          } else {
+            return routine;
+          }
+        })
+      );
+      successCallback();
+      handleClose();
+    });
+  };
 
-              const handleDestroy = (id) => {
-                 console.log("handleDestroy", id);
-                 axios.delete(`http://localhost:3000/routines/${id}.json`).then(() => {
-                   setRoutines(routines.filter((routine) => routine.id !== id));
-                   handleClose();
-                 });
-               };        
-    
-       const handleClose = () => {
-         console.log("handleClose");
-         setIsRoutinesShowVisible(false);
-       };
+  const handleDestroy = (id) => {
+    console.log("handleDestroy", id);
+    axios.delete(`http://localhost:3000/routines/${id}.json`).then(() => {
+      setRoutines(routines.filter((routine) => routine.id !== id));
+      handleClose();
+    });
+  };
+
+  const handleClose = () => {
+    console.log("handleClose");
+    setIsRoutinesShowVisible(false);
+  };
 
   useEffect(() => {
     axios.get("http://localhost:3000/exercises.json").then((response) => {
@@ -71,13 +72,33 @@ export function RoutinesPage() {
 
   useEffect(handleIndex, []);
 
+  const filteredRoutines = routines.filter(routine => {
+    const searchLower = searchQuery.toLowerCase();
+
+    const routineName = routine.name ? routine.name.toLowerCase() : '';
+    const exerciseName = routine.exercise?.name ? routine.exercise.name.toLowerCase() : '';
+
+    return routineName.includes(searchLower) || exerciseName.includes(searchLower);
+  });
+
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
   return (
     <main>
+      <input 
+        type="text" 
+        value={searchQuery} 
+        onChange={handleSearchChange} 
+        placeholder="Search routines..." 
+      />
+      
       <RoutinesNew onCreate={handleCreate} exercises={exercises} />
-      <RoutinesIndex routines={routines} onShow={handleShow} />
+      <RoutinesIndex routines={filteredRoutines} onShow={handleShow} />
       <Modal show={isRoutinesShowVisible} onClose={handleClose}>
-      <RoutinesShow routine={currentRoutine} onUpdate={handleUpdate} onDestroy={handleDestroy} />
-       </Modal>
+        <RoutinesShow routine={currentRoutine} onUpdate={handleUpdate} onDestroy={handleDestroy} />
+      </Modal>
     </main>
   );
 }
